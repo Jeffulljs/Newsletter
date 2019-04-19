7065e81d64e9e85cc485b4f8a51b554a5e7f9f34
Jeff Sapotille <you@example.com>
2019-04-19 21:35:43 +0200

    initialisation du serveur

---
+++ app.js
@@ -0,0 +1,14 @@
+// jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, ()=>{
+    console.log('server started');
+    
+});
