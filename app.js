267366de21b24610efe855c6095a2b99ecf4a211
Jeff Sapotille <you@example.com>
2019-04-19 23:14:01 +0200

    ajout formulaire html

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 // jshint esversion:6
 
+// utilisation de npm express, body-parser, request.
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
+// initialisation de express.
+
 const app = express();
 
+//initialisation de body-parser
+
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+//serveur enregistrer sur localhost 3000.
 
 app.listen(3000, ()=>{
     console.log('server started');
